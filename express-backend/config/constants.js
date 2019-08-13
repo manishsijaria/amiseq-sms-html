d42a0adecf1ecf9f6041320eca256e7b78bd55ef
manishsijaria” <sijariamanish@gmail.com>
2019-08-13 23:54:16 +0530

    Twilio prod. changes

--- express-backend/config/constants.js
+++ express-backend/config/constants.js
@@ -1,9 +1,9 @@
 
 'use strict'
 let CONSTANTS = {
-  TWILIO_SID : process.env.TWILIO_SID,
-  TWILIO_TOKEN : process.env.TWILIO_TOKEN,
-  TWILIO_AMISEQ_NO : process.env.TWILIO_NO,
+  TWILIO_SID : (process.env.NODE_ENV === 'production') ? process.env.TWILIO_SID : process.env.DEMO_TWILIO_SID,
+  TWILIO_TOKEN : (process.env.NODE_ENV === 'production') ? process.env.TWILIO_TOKEN : process.env.DEMO_TWILIO_TOKEN,
+  TWILIO_AMISEQ_NO : (process.env.NODE_ENV === 'production') ? process.env.TWILIO_NO : process.env.DEMO_TWILIO_NO,
   SERVER_PORT : process.env.PORT
 }
 module.exports = Object.freeze(CONSTANTS)  // freeze prevents changes by users
