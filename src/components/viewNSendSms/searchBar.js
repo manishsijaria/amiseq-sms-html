9bbf307ed8635d6be02c8030e4ab5884c3d102ed
manishsijaria” <sijariamanish@gmail.com>
2019-08-27 18:37:27 +0530

    search message text enter bug fixed

--- src/components/viewNSendSms/searchBar.js
+++ src/components/viewNSendSms/searchBar.js
@@ -6,28 +6,27 @@ export default class SearchBar  extends React.Component {
 
     handelChange = (event) => {
         const {  value } = event.target
-        //this.setState({ [name] : value})
         this.props.onSearchTextChange(value)
+        //this.setState({ [name] : value})
     }
+
     render() {
         let rightSplitPaneWidth = this.props.rightSplitPaneWidth
         return(
-            <div style={{display: 'flex', flexDirection: 'row'}}>
-                <div style={{ marginLeft: '10px', marginTop: '15px', 
+            <div style={{display: 'flex', flexDirection: 'row',marginTop: '10px'}}>
+                <div style={{ marginLeft: '10px',  marginTop: '5px',
                               width: `${rightSplitPaneWidth/2}px`}}>
                     {this.props.contactFullname ? 
                         <span> <b>{this.props.contactFullname}</b> added by {this.props.addedByUsername} on {this.props.contactCreateDate} </span>
                         : ''}
                 </div>
-                <div  style={{ width: `${rightSplitPaneWidth/2}px`}}>
-                    <form name="form" className='searchContactForm'>
-                        <input type="text" name="name" 
-                            value = {this.props.filterText} 
-                            onChange={this.handelChange} 
-                            placeholder="Search sms text..."
-                            style={{ width: `${rightSplitPaneWidth/2 - 50}px`}}
-                        />
-                    </form>
+                <div  style={{  width: `${rightSplitPaneWidth/2}px`}}>
+                    <input type="text" name="searchText" 
+                        value = {this.props.filterText} 
+                        onChange={this.handelChange} 
+                        placeholder="Search sms text..."
+                        style={{ width: `${rightSplitPaneWidth/2 - 50}px`}}
+                    />
                 </div> 
             </div>
         )
